f5e4549f158ddd22140df5cb034a38e59979a53d
Rob Kritzinger <kritz.rob@gmail.com>
2022-07-06 21:13:17 +0200

    notes and auth service

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,16 +1,34 @@
-const mock_auth = () =>
-  new Promise((resolve) => {
-    setTimeout(() => resolve(
-      {
-        "_id": "628f8ebbd644b7d4a928e2ca",
-        "name": "John",
-        "surname": "Guy",
-        "username": "John.Guy",
-        "email": "test1@test.com",
-        "bio": "Hi, I'm John, and I like cheese",
-        "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjkzMjMzNmU4MWZkMGNiOTZhNWViMjIiLCJpYXQiOjE2NTUxNTMyNTYsImV4cCI6MTY1NTE1NTA1Nn0.7sVr39YiqWBB3KEOgJp3lAW3sBgWjJtkzsENWMrADhI"
+import config from "../config";
+
+const useAuthenticate = (token) => {
+
+  const [authLoading, setAuthLoading] = useState(false);
+  const [authenticated, setAuthenticated] = useState(false);
+
+  const tryAuth = async () => {
+    try {
+      setAuthLoading(true);
+
+      const res = await axios({
+        method: "POST",
+        url: config.api_url + 'auth/',
+        headers: config.headers(token)
+      });
+
+      if(res.status == 200) {
+        setAuthenticated(true);
+      }
+
+    } catch (e) {
+      console.log(e)
+      setAuthenticated(false);
+    } finally {
+      setAuthLoading(false);
     }
-    ), 250);
-  });
+  }
+
+  return { tryAuth, authLoading, authenticated, isAdmin };
+
+}
 
 export { mock_auth };
